9a1eb012e8ecb71d326cf8582c21e63452614ea1
unknown <mohamedamrrefaay@gmail.com>
2020-12-14 00:38:06 +0200

    add video connection

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 
-const videoArea = document.getElementById("videos-container")
+
+const socket = io("/");
+
+const videoArea = document.getElementById("videos-container");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030"
+})
 
 let myVedioStream;
 navigator.mediaDevices
@@ -11,10 +21,39 @@ navigator.mediaDevices
   })
   .then((stream) => {
     myVedioStream = stream;
-    addVidioStream(myVideo, stream);
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", call => {
+      call.answer(stream)
+      const video = document.createElement("video");
+      call.on("stream", userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+
+    socket.on("user-joined", (userId) => {
+      conneToNewUser(userId, stream);
+    });
   });
 
-const addVidioStream = (video, stream) => {
+
+peer.on("open" , (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+})
+
+
+
+
+const conneToNewUser = (id, stream) => {
+  const call = peer.call(id, stream)
+  const video = document.createElement("video");
+  call.on("stream", userVideoStream => {
+    console.log("streaming")
+    addVideoStream(video, userVideoStream)
+  })
+}
+
+const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
     video.play();
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const { env } = require("process");
 const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require("peer");
+const PORT = process.env.PORT || 3030;
 
-const PORT =  process.env.PORT || 3030;
+const server = app.listen(PORT, () => {
+  console.log("App is running on " + PORT);
+});
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+
+app.use("/peerjs", peerServer)
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
@@ -18,6 +26,13 @@ app.get("/:roomId", (req, res) => {
   res.render("room", { roomId: req.params.roomId });
 });
 
-app.listen(PORT, () => {
-  console.log("App is running on " + PORT);
+const io = require("socket.io")(server);
+
+
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-joined", userId);
+  });
 });
