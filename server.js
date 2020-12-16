972581463578887cc282be42ae2d20b06797ca05
unknown <mohamedamrrefaay@gmail.com>
2020-12-17 01:22:38 +0200

    big enhancements

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 app.use("/peerjs", peerServer);
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.render("index", { uniqueId: uuidv4() });
 });
 
 app.get("/:roomId", (req, res) => {
